c599cf8fa7067f5b6ccd5b60d9d657b08689428b
skyxcripto <producer@skynance.net>
2022-01-10 02:37:34 -0300

    finish

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { BrowserRouter as Router } from 'react-router-dom'
+
+import { Routes } from './routes'
+
+import GlobalStyle from './styles/global'
+
+export default function App() {
+  return (
+    <>
+      <GlobalStyle />
+      <Router>
+        <Routes />
+      </Router>
+    </>
+  )
+}
